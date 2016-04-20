3c1b2680549986dfb6598c93bad8fc9439d31fa4
gutee <mgutierrez@tekgenesis.com>
2016-04-20 17:51:58 -0300

    Drag handler for ABC Order.

---
+++ Assets/Scripts/Common/DragHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    public static GameObject itemBeingDragged;
+    Vector3 startPosition;
+    Transform startParent;
+
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        itemBeingDragged = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        itemBeingDragged = null;
+    }
+}
