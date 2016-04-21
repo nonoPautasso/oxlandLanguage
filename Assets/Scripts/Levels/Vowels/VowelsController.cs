2051eb2390a0c24b74d27872d12c951eb676ccb9
Francisco Digiandomenico <francisco.digiandomenico@gmail.com>
2016-04-21 16:10:53 -0300

    vowelsoral is not broken, spritesheet needs to fixed. added color changing to vowels game when clicking on bubble

--- Assets/Scripts/Levels/Vowels/VowelsController.cs
+++ Assets/Scripts/Levels/Vowels/VowelsController.cs
@@ -7,86 +7,85 @@ using System;
 
 namespace Assets.Scripts.Levels.Vowels
 {
-    public class VowelsController : LevelController
-    {
-        public VowelsView view;
+	public class VowelsController : LevelController
+	{
+		public VowelsView view;
 
-        private VowelsModel model;
+		private VowelsModel model;
 
-        void Start()
-        {
-            InitGame();
-        }
+		void Start ()
+		{
+			InitGame ();
+		}
 
-        public override void RestartGame()
-        {
-            InitGame();
-            view.EnableHint();
-        }
+		public override void RestartGame ()
+		{
+			InitGame ();
+			view.EnableHint ();
+		}
 
-        public override void InitGame()
-        {
-			MetricsManager.instance.GameStart();
-            // Creates model
-            model = new VowelsModel();
-            // Basically sets initial values for model
-            model.StartGame();
-            // Sets all viewable letters to blank
-            for (int i = 0; i < 5; i++)
-            {
-                view.ShowLetter(i, "");
-            }
-            // Generates a letter for each bubble
-            for (int i = 0; i < 4; i++)
-            {
-                view.SetLetterButton(i, model.GenerateLetter());
-            }
-        }
+		public override void InitGame ()
+		{
+			MetricsManager.instance.GameStart ();
+			// Creates model
+			model = new VowelsModel ();
+			// Basically sets initial values for model
+			model.StartGame ();
+			// Sets all viewable letters to blank
+			for (int i = 0; i < 5; i++) {
+				view.ShowLetter (i, "");
+			}
+			// Generates a letter for each bubble
+			for (int i = 0; i < 4; i++) {
+				view.SetLetterButton (i, model.GenerateLetter ());
+			}
+		}
 
 
-        public override void NextChallenge()
-        {
+		public override void NextChallenge ()
+		{
 
-        }
+		}
 
-        public override void ShowHint()
-        {
-            // Asks model for all the vowels that haven't been revealed + their index
-            DataPair<string, int>[] letters = model.RequestHintInfo();
-            for (int i = 0; i < letters.Length; i++)
-            {
-                // Calls on the view to show the letter as hint
-                view.RevealHint(letters[i].Snd(), letters[i].Fst());
-            }
-            view.DisableHint();
-        }
+		public override void ShowHint ()
+		{
+			// Asks model for all the vowels that haven't been revealed + their index
+			DataPair<string, int>[] letters = model.RequestHintInfo ();
+			for (int i = 0; i < letters.Length; i++) {
+				// Calls on the view to show the letter as hint
+				view.RevealHint (letters [i].Snd (), letters [i].Fst ());
+			}
+			view.DisableHint ();
+		}
 
 
-        public void SubmitLetter(Text letter)
-        {
-            // Returns the index in the revealed letter space for the clicked letter
-            int index = model.IndexOfRevealedVowel(letter.text);
-            if (index != -1)
-            {
-                view.PlayRightSound();
-                // Clicked letter is a vowel
-                view.ShowLetter(index, letter.text);    // Tells view to show the letter at appropriate index
-                model.RevealLetter(index); // Tells model to record that the vowel was revealed
-               // LogAnswer(true);
-            }
-            else {
-                //  LogAnswer(false);
-                view.PlayWrongSound();
-            }
-            if (model.GetNumRevealedLetters() == 5) EndGame(model.MinSeconds, model.PointsPerSecond, model.PointsPerError);
-        }
+		public void SubmitLetter (Button bubble)
+		{
+			// Returns the index in the revealed letter space for the clicked letter
+			int index = model.IndexOfRevealedVowel (bubble.GetComponentInChildren<Text> ().text);
+			if (index != -1) {
+				view.SetBubbleCorrect (bubble);
+				view.PlayRightSound ();
+				// Clicked letter is a vowel
+				view.ShowLetter (index, bubble.GetComponentInChildren<Text> ().text);    // Tells view to show the letter at appropriate index
+				model.RevealLetter (index); // Tells model to record that the vowel was revealed
+				LogAnswer (true);
+			} else {
+				view.SetBubbleWrong (bubble);
+				LogAnswer (false);
+				view.PlayWrongSound ();
+			}
+			if (model.GetNumRevealedLetters () == 5)
+				EndGame (model.MinSeconds, model.PointsPerSecond, model.PointsPerError);
+			ResetBubble (bubble);
+		}
 
-        public void ResetBubble(Button bubble)
-        {
-            string letter = model.GenerateLetter(); // Generate a new letter for the bubble
-            model.ReturnToStack(bubble.GetComponentInChildren<Text>().text);    // Returns the previous letter to the model stack
-            view.ResetAnimation(bubble, letter);    // Tell view to make bubble start at bottom again with new letter
-        }
+		public void ResetBubble (Button bubble)
+		{
+			string letter = model.GenerateLetter (); // Generate a new letter for the bubble
+			model.ReturnToStack (bubble.GetComponentInChildren<Text> ().text);    // Returns the previous letter to the model stack
+			view.ResetAnimation (bubble, letter);    // Tell view to make bubble start at bottom again with new letter
+		}
 
-    }
+	}
 }
