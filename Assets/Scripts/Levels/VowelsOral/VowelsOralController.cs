220cac60efa6c59772861c02528595dde699e80a
Francisco Digiandomenico <francisco.digiandomenico@gmail.com>
2016-04-25 15:41:52 -0300

    added path for englush in vowelssounds. just one small error, missing image for oil, or extra audio oil

--- Assets/Scripts/Levels/VowelsOral/VowelsOralController.cs
+++ Assets/Scripts/Levels/VowelsOral/VowelsOralController.cs
@@ -6,72 +6,71 @@ using UnityEngine.UI;
 
 namespace Assets.Scripts.Levels.VowelsOral
 {
-    public class VowelsOralController : LevelController
-    {
+	public class VowelsOralController : LevelController
+	{
 
-        public VowelsOralView view;
+		public VowelsOralView view;
 
-        private VowelsOralModel model;
+		private VowelsOralModel model;
 
-        public override void InitGame()
-        {
-			MetricsManager.instance.GameStart();
-            model = new VowelsOralModel();
-            model.InitModel();
-            view.InitView();
-            LoadResources();
-            view.Next(model.Next());
-        }
+		public override void InitGame ()
+		{
+			MetricsManager.instance.GameStart ();
+			model = new VowelsOralModel ();
+			model.InitModel ();
+			view.InitView ();
+			LoadResources ();
+			view.Next (model.Next ());
+		}
 
-        public override void NextChallenge()
-        {
-            view.Next(model.Next());
-        }
+		public override void NextChallenge ()
+		{
+			view.Next (model.Next ());
+		}
 
-        public override void RestartGame()
-        {
-            throw new NotImplementedException();
-        }
+		public override void RestartGame ()
+		{
+			throw new NotImplementedException ();
+		}
 
-        public override void ShowHint()
-        {
-            view.ShowHints(model.RequestHintInfo());
-        }
+		public override void ShowHint ()
+		{
+			view.ShowHints (model.RequestHintInfo ());
+		}
 
-        public void LoadResources()
-        {
-            view.SetResources(model.LoadResources("A"));
-            view.SetResources(model.LoadResources("E"));
-            view.SetResources(model.LoadResources("I"));
-            view.SetResources(model.LoadResources("O"));
-            view.SetResources(model.LoadResources("U"));
-        }
+		public void LoadResources ()
+		{
+			int language = SettingsController.instance.GetLanguage ();
+			view.SetResources (model.LoadResources ("A", language));
+			view.SetResources (model.LoadResources ("E", language));
+			view.SetResources (model.LoadResources ("I", language));
+			view.SetResources (model.LoadResources ("O", language));
+			view.SetResources (model.LoadResources ("U", language));
+		}
 
-        // Use this for initialization
-        void Start()
-        {
-            InitGame();
-        }
+		// Use this for initialization
+		void Start ()
+		{
+			InitGame ();
+		}
 
        
 
-        public void SubmitLetter(Button button)
-        {
-            bool correct = model.CheckSubmittedLetter(button.GetComponentInChildren<Text>().text);
-            if (correct)
-            {
-                view.PlayRightSound();
-                  LogAnswer(true);
-                int nextLetterIndex = model.Next();
-                if (nextLetterIndex == -1) EndGame(model.MinSeconds, model.PointsPerSecond, model.PointsPerError);
-                view.Next(nextLetterIndex);    // Tells view to show the letter at appropriate index
-            }
-            else
-            {
-                view.PlayWrongSound();
-                LogAnswer(false);
-            }
-        }
+		public void SubmitLetter (Button button)
+		{
+			bool correct = model.CheckSubmittedLetter (button.GetComponentInChildren<Text> ().text);
+			if (correct) {
+				view.PlayRightSound ();
+				LogAnswer (true);
+				int nextLetterIndex = model.Next ();
+				if (nextLetterIndex == -1)
+					EndGame (model.MinSeconds, model.PointsPerSecond, model.PointsPerError);
+				view.Next (nextLetterIndex);    // Tells view to show the letter at appropriate index
+			} else {
+				view.PlayWrongSound ();
+				LogAnswer (false);
+			}
+		}
 
-    }
+	}
 }
--- Assets/Scripts/Levels/VowelsOral/VowelsOralModel.cs
+++ Assets/Scripts/Levels/VowelsOral/VowelsOralModel.cs
@@ -8,212 +8,260 @@ using System.Collections.Generic;
 
 namespace Assets.Scripts.Levels.VowelsOral
 {
-    public class VowelsOralModel : LevelModel
-    {
+	public class VowelsOralModel : LevelModel
+	{
 
-        private string currentCorrectLetter;
+		private string currentCorrectLetter;
 
-        int[] letterAmounts;
+		int[] letterAmounts;
+		int langNum;
 
-        public void InitModel()
-        {
-            letterAmounts = new int[5];
-        }
+		public void InitModel ()
+		{
+			letterAmounts = new int[5];
+		}
 
-        public DataTrio<Sprite[], AudioClip[], int> LoadResources(string letter)
-        {
-            int intValue;
-            switch (letter)
-            {
-                case "A":
-                    intValue = 0;
-                    break;
-                case "E":
-                    intValue = 1;
-                    break;
-                case "I":
-                    intValue = 2;
-                    break;
-                case "O":
-                    intValue = 3;
-                    break;
-                case "U":
-                    intValue = 4;
-                    break;
-                default:
-                    intValue = -1;
-                    break;
-            }
-            DataTrio<Sprite[], AudioClip[], int> toReturn;
-            AudioClip[] clips = Resources.LoadAll<AudioClip>("Audio/Spanish/" + letter + "Words");
-            Sprite[] images = Resources.LoadAll<Sprite>("Sprites/Spanish/ObjectsVowels");
-            toReturn = new DataTrio<Sprite[], AudioClip[], int>(new Sprite[clips.Length], new AudioClip[clips.Length], intValue);
-            for (int i = 0, j = GetLetterStartingIndex(letter); j < GetLetterStartingIndex(letter)
-                + GetLetterSize(letter); i++, j++)
-            {
-                toReturn.Fst()[i] = images[j];
-                toReturn.Snd()[i] = clips[i];
-            }
-            letterAmounts[intValue] = 2;
-            return toReturn;
-        }
+		public DataTrio<Sprite[], AudioClip[], int> LoadResources (string letter, int language)
+		{
+			Debug.Log ("Entered LoadResources method with letter: " + letter);
+			langNum = language;
+			string lang;
+			string englishText;
+			switch (language) {
+			case 0:
+				lang = "Spanish";
+				englishText = "";
+				break;
+			case 1:
+				lang = "English";
+				englishText = "English";
+				break;
+			default:
+				lang = "Spanish";
+				Debug.Log ("Default language set");
+				englishText = "";
+				break;
+			}
+			int intValue;
+			switch (letter) {
+			case "A":
+				intValue = 0;
+				break;
+			case "E":
+				intValue = 1;
+				break;
+			case "I":
+				intValue = 2;
+				break;
+			case "O":
+				intValue = 3;
+				break;
+			case "U":
+				intValue = 4;
+				break;
+			default:
+				intValue = -1;
+				break;
+			}
+			DataTrio<Sprite[], AudioClip[], int> toReturn;
+			AudioClip[] clips = Resources.LoadAll<AudioClip> ("Audio/" + lang + "/" + letter + "Words");
+			Sprite[] images = Resources.LoadAll<Sprite> ("Sprites/" + lang + "/ObjectsVowels" + englishText);
+			Debug.Log ("Clips length: " + clips.Length);
+			Debug.Log ("Images length: " + images.Length);
+			toReturn = new DataTrio<Sprite[], AudioClip[], int> (new Sprite[clips.Length], new AudioClip[clips.Length], intValue);
+			for (int i = 0, j = GetLetterStartingIndex (letter); j < GetLetterStartingIndex (letter)
+			+ GetLetterSize (letter); i++, j++) {
+				toReturn.Fst () [i] = images [j];
+				toReturn.Snd () [i] = clips [i];
+			}
+			letterAmounts [intValue] = 2;
+			return toReturn;
+		}
 
-        public override void NextChallenge()
-        {
-            throw new NotImplementedException();
-        }
+		public override void NextChallenge ()
+		{
+			throw new NotImplementedException ();
+		}
 
-        public override void RequestHint()
-        {
-            throw new NotImplementedException();
-        }
+		public override void RequestHint ()
+		{
+			throw new NotImplementedException ();
+		}
 
-        public DataPair<int, int> RequestHintInfo()
-        {
-            DataPair<int, int> toReturn;
-            switch (currentCorrectLetter)
-            {
-                case "A":
-                    toReturn = new DataPair<int, int>(2, 4);
-                    break;
-                case "E":
-                    toReturn = new DataPair<int, int>(2, 4);
-                    break;
-                case "I":
-                    toReturn = ChooseTwoRandomOpenVowels();
-                    break;
-                case "O":
-                    toReturn = new DataPair<int, int>(2, 4);
-                    break;
-                case "U":
-                    toReturn = ChooseTwoRandomOpenVowels();
-                    break;
-                default:
-                    toReturn = null;
-                    break;
-            }
-            return toReturn;
-        }
+		public DataPair<int, int> RequestHintInfo ()
+		{
+			DataPair<int, int> toReturn;
+			switch (currentCorrectLetter) {
+			case "A":
+				toReturn = new DataPair<int, int> (2, 4);
+				break;
+			case "E":
+				toReturn = new DataPair<int, int> (2, 4);
+				break;
+			case "I":
+				toReturn = ChooseTwoRandomOpenVowels ();
+				break;
+			case "O":
+				toReturn = new DataPair<int, int> (2, 4);
+				break;
+			case "U":
+				toReturn = ChooseTwoRandomOpenVowels ();
+				break;
+			default:
+				toReturn = null;
+				break;
+			}
+			return toReturn;
+		}
 
-        public DataPair<int, int> ChooseTwoRandomOpenVowels()
-        {
-            DataPair<int, int> toReturn;
-            int fst = 0, snd = 0;
-            while (fst == snd)
-            {
-                fst = UnityEngine.Random.Range(0, 3);
-                snd = UnityEngine.Random.Range(0, 3);
-            }
-            if (fst == 2) fst = 3;
-            if (snd == 2) snd = 3;
-            toReturn = new DataPair<int, int>(fst, snd);
-            return toReturn;
-        }
+		public DataPair<int, int> ChooseTwoRandomOpenVowels ()
+		{
+			DataPair<int, int> toReturn;
+			int fst = 0, snd = 0;
+			while (fst == snd) {
+				fst = UnityEngine.Random.Range (0, 3);
+				snd = UnityEngine.Random.Range (0, 3);
+			}
+			if (fst == 2)
+				fst = 3;
+			if (snd == 2)
+				snd = 3;
+			toReturn = new DataPair<int, int> (fst, snd);
+			return toReturn;
+		}
 
-        public override void StartGame()
-        {
-            throw new NotImplementedException();
-        }
+		public override void StartGame ()
+		{
+			throw new NotImplementedException ();
+		}
 
-        static void RandomizeArray<T>(T[] arr)
-        {
-            for (var i = arr.Length - 1; i > 0; i--)
-            {
-                var r = UnityEngine.Random.Range(0, i);
-                var tmp = arr[i];
-                arr[i] = arr[r];
-                arr[r] = tmp;
-            }
-        }
+		static void RandomizeArray<T> (T[] arr)
+		{
+			for (var i = arr.Length - 1; i > 0; i--) {
+				var r = UnityEngine.Random.Range (0, i);
+				var tmp = arr [i];
+				arr [i] = arr [r];
+				arr [r] = tmp;
+			}
+		}
 
-        public int Next()
-        {
-            if (letterAmounts[0] == 0 && letterAmounts[1] == 0 && letterAmounts[2] == 0
-                && letterAmounts[3] == 0 && letterAmounts[4] == 0) return -1;
-            int value = UnityEngine.Random.Range(0, 5);
-            if (letterAmounts[value] == 0) return Next();
-            else letterAmounts[value]--;
-            switch (value)
-            {
-                case 0:
-                    currentCorrectLetter = "A";
-                    break;
-                case 1:
-                    currentCorrectLetter = "E";
-                    break;
-                case 2:
-                    currentCorrectLetter = "I";
-                    break;
-                case 3:
-                    currentCorrectLetter = "O";
-                    break;
-                case 4:
-                    currentCorrectLetter = "U";
-                    break;
-                default:
-                    currentCorrectLetter = null;
-                    break;
-            }
-            return value;
-        }
+		public int Next ()
+		{
+			if (letterAmounts [0] == 0 && letterAmounts [1] == 0 && letterAmounts [2] == 0
+			    && letterAmounts [3] == 0 && letterAmounts [4] == 0)
+				return -1;
+			int value = UnityEngine.Random.Range (0, 5);
+			if (letterAmounts [value] == 0)
+				return Next ();
+			else
+				letterAmounts [value]--;
+			switch (value) {
+			case 0:
+				currentCorrectLetter = "A";
+				break;
+			case 1:
+				currentCorrectLetter = "E";
+				break;
+			case 2:
+				currentCorrectLetter = "I";
+				break;
+			case 3:
+				currentCorrectLetter = "O";
+				break;
+			case 4:
+				currentCorrectLetter = "U";
+				break;
+			default:
+				currentCorrectLetter = null;
+				break;
+			}
+			return value;
+		}
 
-        public bool CheckSubmittedLetter(string letter)
-        {
-            return letter.Equals(currentCorrectLetter);
-        }
+		public bool CheckSubmittedLetter (string letter)
+		{
+			return letter.Equals (currentCorrectLetter);
+		}
 
-        int GetLetterStartingIndex(string letter)
-        {
-            int toReturn;
-            switch (letter)
-            {
-                case "A":
-                    toReturn = 0;
-                    break;
-                case "E":
-                    toReturn = 13;
-                    break;
-                case "I":
-                    toReturn = 24;
-                    break;
-                case "O":
-                    toReturn = 29;
-                    break;
-                case "U":
-                    toReturn = 36;
-                    break;
-                default:
-                    toReturn = -1;
-                    break;
-            }
-            return toReturn;
-        }
+		int GetLetterStartingIndex (string letter)
+		{
+			Debug.Log (letter);
+			int toReturn;
+			switch (letter) {
+			case "A":
+				toReturn = 0;
+				break;
+			case "E":
+				if (langNum == 0)
+					toReturn = 13;
+				else
+					toReturn = 6;
+				break;
+			case "I":
+				if (langNum == 0)
+					toReturn = 24;
+				else
+					toReturn = 13;
+				break;
+			case "O":
+				if (langNum == 0)
+					toReturn = 29;
+				else
+					toReturn = 19;
+				break;
+			case "U":
+				if (langNum == 0)
+					toReturn = 36;
+				else
+					toReturn = 26;
+				break;
+			default:
+				toReturn = -1;
+				break;
+			}
+			Debug.Log (toReturn);
+			return toReturn;
+		}
 
-        int GetLetterSize(string letter)
-        {
-            int toReturn;
-            switch (letter)
-            {
-                case "A":
-                    toReturn = 13;
-                    break;
-                case "E":
-                    toReturn = 11;
-                    break;
-                case "I":
-                    toReturn = 5;
-                    break;
-                case "O":
-                    toReturn = 7;
-                    break;
-                case "U":
-                    toReturn = 5;
-                    break;
-                default:
-                    toReturn = -1;
-                    break;
-            }
-            return toReturn;
-        }
-    }
+		int GetLetterSize (string letter)
+		{
+			int toReturn;
+			switch (letter) {
+			case "A":
+				if (langNum == 0)
+					toReturn = 13;
+				else
+					toReturn = 6;
+				break;
+			case "E":
+				if (langNum == 0)
+					toReturn = 11;
+				else
+					toReturn = 7;
+				break;
+			case "I":
+				if (langNum == 0)
+					toReturn = 5;
+				else
+					toReturn = 6;
+				break;
+			case "O":
+				if (langNum == 0)
+					toReturn = 7;
+				else
+					toReturn = 8;
+				break;
+			case "U":
+				if (langNum == 0)
+					toReturn = 5;
+				else
+					toReturn = 5;
+				break;
+			default:
+				toReturn = -1;
+				break;
+			}
+			return toReturn;
+		}
+	}
 }
