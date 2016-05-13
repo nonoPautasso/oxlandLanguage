50495eab2babbd5c8509d3075c8df716e0b2f7a0
gutee <mgutierrez@tekgenesis.com>
2016-05-13 09:53:35 -0300

    Split sentences half way done

--- Assets/Scripts/Levels/SplitSentences/SplitSentencesView.cs
+++ Assets/Scripts/Levels/SplitSentences/SplitSentencesView.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Assets.Scripts.Common;
+using UnityEditor;
 
 namespace Assets.Scripts.Levels.SplitSentences {
 	public class SplitSentencesView : LevelView {
@@ -11,11 +12,21 @@ namespace Assets.Scripts.Levels.SplitSentences {
 		public Image lettersPanel;
 		public Image sentencePanel;
 
+		private static List<Color> colors = new List<Color> {Color.blue, Color.green, Color.red, Color.yellow, Color.cyan, Color.magenta};
+		public Randomizer colorRandomizer;
+		public Color currentColor;
+
+		private bool paintMode;
+
+		public static int MAX_CHARS = 30;
+
 		private SplitSentencesController controller;
 
 		public void NextChallenge (string sentence) {
 			ResetView ();
 			SetSentence (sentence);
+			currentColor = Color.white;
+			colorRandomizer = Randomizer.New (colors.Count - 1);
 		}
 
 		private void SetSentence (string sentence) {
@@ -26,16 +37,11 @@ namespace Assets.Scripts.Levels.SplitSentences {
 				if (charNumber < charArray.Length){
 					ResetLetterToggle (letterToggle);
 					letterToggle.GetComponentInChildren<Text>().text = charArray[charNumber].ToString ();
-					AddEvents (letterToggle);
 				} else {
 					Views.SetActiveToggle (letterToggle, false);
 				}
 				charNumber++;
 			}
-		}
-
-		private void AddEvents (Toggle letterToggle) {
-			
 		}
 
 		void ResetLetterToggle (Toggle letterToggle) {
@@ -47,12 +53,108 @@ namespace Assets.Scripts.Levels.SplitSentences {
 			controller.ShowHint ();
 		}
 
+		public void PaintModeOn (TogglePaint togglePaint) {
+			paintMode = true;
+		}
+
+		public void PaintModeOff (TogglePaint togglePaint) {
+			paintMode = false;
+			currentColor = Color.white;
+
+			RefreshWords ();
+		}
+
+		private void RefreshWords () {
+			ClearWordsAndLines ();
+			AddColoredWords ();
+		}
+
+		private void ClearWordsAndLines () {
+			foreach (GameObject line in sentenceLines) {
+				foreach (Button word in line.GetComponentsInChildren<Button>()) {
+					Destroy (word);
+				}
+				if (line != sentenceLines [0]) Destroy (line);
+			}
+		}
+
+		private void AddColoredWords () {
+			TogglePaint[] togglePaints = lettersPanel.GetComponentsInChildren<TogglePaint> ();
+			List<string> words = new List<string> ();
+			string currentWord = "";
+			Color currentColor = togglePaints[0].GetColor ();
+			foreach (TogglePaint toggle in togglePaints) {
+				if(toggle.GetColor () == currentColor && currentColor != Color.white) {
+					currentWord += toggle.GetComponentInChildren <Text> ().text;
+				}
+				else {
+					currentColor = toggle.GetColor ();
+					if (currentWord.Length != 0) words.Add (currentWord);
+					currentWord = "";
+					if (currentColor != Color.white) currentWord = toggle.GetComponentInChildren <Text> ().text;
+				}
+			}
+			if (currentWord.Length != 0) words.Add (currentWord);
+
+			int lineCount = 0;
+			int charCount = 0;
+			foreach (string w in words) {
+				if (charCount + w.Length > MAX_CHARS) {
+					lineCount++;
+					charCount = 0;
+				}
+				GameObject line = sentenceLines.Count == lineCount ? NewLine(sentenceLines, sentencePanel) : sentenceLines [lineCount];
+				GameObject word = GetWord ();
+				word.GetComponentInChildren<Text>().text = w;
+				word.transform.SetParent (line.transform, true);
+				word.transform.localScale = Vector3.one;
+
+				word.GetComponent <Button> ().onClick.AddListener (() => WordClick (word));
+				charCount += w.Length;
+			}
+		}
+
+		public void WordClick(GameObject word){
+			PlaySoundClic ();
+			Destroy (word);
+			//todo unpaint letters.
+		}
+
+		private GameObject NewLine (List<GameObject> lines, Image panel) {
+			GameObject line = GetLine ();
+			line.transform.SetParent (panel.transform, true);
+			line.transform.localScale = Vector3.one;
+			lines.Add (line);
+			return line;
+		}
+
+		public GameObject GetWord(){
+			return Instantiate<GameObject>(LoadPrefab("Levels/CreateSentence/wordButton"));
+		}
+
+		public GameObject GetLine(){
+			return Instantiate<GameObject>(LoadPrefab("Levels/MayusculasMinusculas/SentenceLine"));
+		}
+
+		private GameObject LoadPrefab(string name) {
+			return Resources.Load<GameObject>("Prefabs/" + name);
+		}
+
 		public void ResetView () {
-			
+			ClearWordsAndLines ();
 		}
 
 		public void TryClick(){
 			
+		}
+
+		public bool PaintMode () {
+			return paintMode;
+		}
+
+		public Color GetColor () {
+			if (currentColor == Color.white) currentColor = colors [colorRandomizer.Next ()];
+			return currentColor;
 		}
 
 		public void Controller (SplitSentencesController controller) { this.controller = controller; }
---
+++ Assets/Scripts/Levels/SplitSentences/TogglePaint.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Levels.CreateSentences;
+using Assets.Scripts.Levels.SplitSentences;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TogglePaint : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler {
+	public SplitSentencesView view;
+
+	public void OnPointerDown (PointerEventData eventData) {
+		if (!AmIPainted ()) {
+			view.PaintModeOn (this);
+			PaintMe (view.GetColor ());
+		}
+	}
+
+	public void OnPointerUp (PointerEventData eventData) {
+		view.PaintModeOff (this);
+	}
+
+	public void OnPointerEnter (PointerEventData eventData) {
+		if(view.PaintMode()) {
+			if (AmIPainted ())
+				view.PaintModeOff (this);
+			else {
+				PaintMe (view.GetColor ());
+			}
+		}
+	}
+
+	private void PaintMe (Color c) {
+		GetComponentInChildren<Image>().color = c;
+	}
+
+	private bool AmIPainted () {
+		return GetComponentInChildren<Image> ().color != Color.white;
+	}
+
+	public Color GetColor(){
+		return GetComponentInChildren<Image> ().color;
+	}
+}
