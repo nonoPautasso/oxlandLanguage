6f8958d741c193fe2010b801e689ee6c78a38f24
marycraig <marycraig@oxboard.com>
2016-06-22 19:01:37 -0300

    4 new games

--- Assets/Scripts/App/controller/ViewController.cs
+++ Assets/Scripts/App/controller/ViewController.cs
@@ -35,6 +35,7 @@ namespace Assets.Scripts.App{
 				Destroy(gameObject);
 
 			DontDestroyOnLoad(transform.root.gameObject);
+			//TODO: UNCOMMENT FOR REAL GAME ORDEN
 //			levels = new string[]{"Vowels","VowelsOral","StartWithVowel","CompleteVowel","OracionesPictogramas",
 //				"Consonants","ConsonantsOral","ABCWords","CompleteConsonant","ABCOrder",
 //				"CountLetters","CombineSounds","SoundsInWords",
@@ -42,8 +43,9 @@ namespace Assets.Scripts.App{
 //				"MayusMin","SplitSentences","IdentifyLettersInWords","ABCBonus","Syllables"
 //				};
 
-			levels = new string[]{"CreateSentence","CountLetters","StartWithVowel","CompleteVowel","OracionesPictogramas",
-				"Consonants","ConsonantsOral","ABCWords","CompleteConsonant","ABCOrder",
+			//TEMPORARY ORDER FOR TESTING PURPOSES ONLY
+			levels = new string[]{"LettersComposeWords","CountLetters","IdentifyInitialSound","MemotestEnding","OracionesPictogramas",
+				"FindError","ConsonantsOral","ABCWords","CompleteConsonant","ABCOrder",
 				"CountLetters","CombineSounds","SoundsInWords","LettersComposeWords","OrderLetters",
 				"ListenAndWrite","FindError","CreateSentence",
 				"MayusMin","SplitSentences","IdentifyLettersInWords","ABCBonus","Syllables"
